faa18974a48642b3438362588dfad63caf0066ca
Anna Melnyk <blairw73@gmail.com>
2025-01-12 23:21:50 +1100

    Add changes

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,5 +1,5 @@
 import { appConfig } from './../app.config';
-import {Injectable} from "@angular/core";
+import { Injectable, resource } from '@angular/core';
 import {environment} from "../../environments/environment.development";
 import {Course} from "../models/course.model";
 import { Response } from 'express';
@@ -17,6 +17,38 @@ export class CoursesServiceWithFetch {
     const payload = await response.json()
 
     return payload.courses
+  }
+
+  async createNewCourse(course: Partial<Course>): Promise<Course> {
+   const response = await fetch(`${this.env.apiRoot}/courses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+   })
+    
+    const createdCourse = await response.json()
+    
+    return createdCourse
+  }
+
+  async saveCourse(courseId: string, course: Partial<Course>): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+   })
+    
+    return response.json()    
+  }
+
+  async deleteCourse(courseId: string): Promise<void> {
+    await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'DELETE'
+    }) 
   }
 
 
