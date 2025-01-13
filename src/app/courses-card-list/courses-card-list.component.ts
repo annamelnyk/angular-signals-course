ddd63738aaaf9e890559c9d3fb3b21a6db53fc3c
Anna Melnyk <blairw73@gmail.com>
2025-01-13 23:43:24 +1100

    Edit modal

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, input, output} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Course} from "../models/course.model";
 import {MatDialog} from "@angular/material/dialog";
+import {EditCourseDialogComponent, openEditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component'
 
 @Component({
     selector: 'courses-card-list',
@@ -15,8 +16,22 @@ export class CoursesCardListComponent {
     courses = input.required<Course[]>({
         alias: 'inputData'
     })
-    onDeleteCourse = output(e)
-    
+    onDeleteCourse = output<Course>()
+    dialog = inject(MatDialog)
+
+    async onEditCourse(course: Course) {
+        const updatedCourse = await openEditCourseDialogComponent(this.dialog, {
+            mode: 'update',
+            title: 'Edit Course',
+            course
+        })
+
+        console.log({updatedCourse})
+        if(updatedCourse) {
+            await this.onEditCourse(updatedCourse)
+        }
+    }
+
     removeCourse(course: Course) {
         this.onDeleteCourse.emit(course)
 
--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,12 +1,14 @@
 import {Component, effect, inject, signal} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
 import {Course} from "../models/course.model";
-import {EditCourseDialogData} from "./edit-course-dialog.data.model";
+import { EditCourseDialogData } from './edit-course-dialog.data.model';
 import {CoursesService} from "../services/courses.service";
 import {LoadingIndicatorComponent} from "../loading/loading.component";
 import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
 import {CourseCategory} from "../models/course-category.model";
+import {firstValueFrom} from 'rxjs'
+import { saveCourse } from '../../../server/save-course.route';
 
 @Component({
   selector: 'edit-course-dialog',
@@ -20,6 +22,44 @@ import {CourseCategory} from "../models/course-category.model";
   styleUrl: './edit-course-dialog.component.scss'
 })
 export class EditCourseDialogComponent {
+  dialogRef = inject(MatDialogRef)
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA)
+  fb = inject(FormBuilder)
+  
+  form = this.fb.group({
+    title: [''],
+    longDescription: [''],
+    category: [''],
+    image: [''],
+  })
 
+  constructor() {
+    this.form.patchValue({
+      title: this.data.course?.title,
+      longDescription: this.data.course?.longDescription,
+      category: this.data.course?.category,
+      image: this.data.course?.iconUrl
+    })
+  }
 
+  onClose() {
+    this.dialogRef.close()
+  }
+
+  saveCourse() {
+
+  }
+}
+
+export async function openEditCourseDialogComponent(dialog: MatDialog, data: EditCourseDialogData) {
+  const config = new MatDialogConfig()
+  
+  config.autoFocus = true
+  config.width = '400px'
+  config.data = data
+
+  const close$ = dialog.open(EditCourseDialogComponent, config)
+    .afterClosed()
+  
+  return firstValueFrom(close$)
 }
